27b2e06b22136ae96eea017ddfad3f67e6c1f9a3
akram3598786 <akramali03598786@gmail.com>
2022-11-24 12:19:17 +0530

    Signup and login at backend

---
+++ Revesion/MERN-APP/Backend/Middlewares/Auth.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const UserModel = require("../Modals/User.model");
+const AuthRouter = express.Router();
+
+AuthRouter.post("/signup",SignUp);
+AuthRouter.post("/login",Login);
+
+async function SignUp(req, res){
+    try{
+       const userData = req.body;
+       await UserModel.create(userData);
+       let user = await UserModel.find({email : userData.email},{name: 1});
+       //console.log(user)
+       res.status(201).send({message : `${user[0].name} Registed`, user})
+    }
+    catch(err){
+        res.status(500).send(err.message)
+    }
+}
+
+async function Login(req, res){
+    let token = 2345678
+ try{
+   const payload = req.body;
+   let foundUser = await UserModel.findOne(payload);
+   foundUser = foundUser.toJSON();
+   delete foundUser.password;
+   console.log(foundUser)
+   res.status(201).send({message : `${foundUser.name} logged in successfully`, token, foundUser })
+ }
+ catch(err){
+    res.status(500).send(err.message)
+  } 
+}
+
+module.exports = AuthRouter;
---
+++ Revesion/MERN-APP/Backend/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const ConnectDB  = require("./Config/ConnectDB.js");
+const AuthRouter = require("./Middlewares/Auth.js");
+
+const app = express();
+
+app.use(express.json());
+app.use("/auth",AuthRouter);
+let port = 8080;
+app.get("/",(rq, res)=>[
+    res.send("Its working fine")
+])
+app.listen(port, async (req, res)=>{
+    try{
+       await ConnectDB();
+       console.log("server running on 8080 port")
+    }
+    catch(err){
+      console.log("Error occured", err);
+    }
+})
