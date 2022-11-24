fa773cd1537b36d9a088fd057bfe4aa9f1dc37c4
akram3598786 <akramali03598786@gmail.com>
2022-11-24 20:39:32 +0530

    Post create and accsess built with relation with user

--- Revesion/MERN-APP/Backend/Models/Post.model.js
+++ Revesion/MERN-APP/Backend/Models/Post.model.js
@@ -4,6 +4,7 @@ const PostSchema = new mongoose.Schema(
     {
         title : {type : String, require : true},
         description : {type : String},
+        createdby : {type : String, require : true}
     },{
         timestamps : true,
         versionKey : false
